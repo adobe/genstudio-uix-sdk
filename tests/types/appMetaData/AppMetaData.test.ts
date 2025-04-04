ac73951ffa3d78e287d82a4ebeef3c0a4cda13d0
Ewocker <chockerlin@gmail.com>
2025-04-04 14:00:00 -0700

    add test for AppMetadata

--- tests/types/appMetaData/AppMetaData.test.ts
+++ tests/types/appMetaData/AppMetaData.test.ts
@@ -10,7 +10,10 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-import { AppMetaData } from "../../../src/types/appMetaData/AppMetaData";
+import {
+  AppMetadata,
+  AppMetaData,
+} from "../../../src/types/appMetaData/AppMetaData";
 import { Email, Display, Meta } from "../../../src/types/channel/Channel";
 
 describe("contract", () => {
@@ -58,5 +61,51 @@ describe("contract", () => {
     expect(appMetaData.supportedChannels[2].id).toBe("display");
     expect(appMetaData.supportedChannels[2].name).toBe("Display");
     expect(appMetaData.extensionId).toBe("extensionId");
+  });
+
+  it("should define AppMetadata", () => {
+    const AppMetadata: AppMetadata = {
+      id: "1234",
+      label: "label",
+      extensionId: "extensionId",
+      iconDataUri: "iconDataUri",
+      supportedChannels: [
+        {
+          id: "Email",
+          name: "Email",
+        },
+      ],
+    };
+    expect(AppMetadata).toBeDefined();
+    expect(AppMetadata.id).toBe("1234");
+    expect(AppMetadata.label).toBe("label");
+    expect(AppMetadata.iconDataUri).toBe("iconDataUri");
+    expect(AppMetadata.supportedChannels).toBeDefined();
+    expect(AppMetadata.supportedChannels.length).toBe(1);
+    expect(AppMetadata.supportedChannels[0].id).toBe("Email");
+    expect(AppMetadata.extensionId).toBe("extensionId");
+  });
+
+  it("should use Email, Display, and Meta as a channel", () => {
+    const AppMetadata: AppMetadata = {
+      id: "1234",
+      label: "label",
+      extensionId: "extensionId",
+      iconDataUri: "iconDataUri",
+      supportedChannels: [Email, Meta, Display],
+    };
+    expect(AppMetadata).toBeDefined();
+    expect(AppMetadata.id).toBe("1234");
+    expect(AppMetadata.label).toBe("label");
+    expect(AppMetadata.iconDataUri).toBe("iconDataUri");
+    expect(AppMetadata.supportedChannels).toBeDefined();
+    expect(AppMetadata.supportedChannels.length).toBe(3);
+    expect(AppMetadata.supportedChannels[0].id).toBe("email");
+    expect(AppMetadata.supportedChannels[0].name).toBe("Email");
+    expect(AppMetadata.supportedChannels[1].id).toBe("meta");
+    expect(AppMetadata.supportedChannels[1].name).toBe("Meta");
+    expect(AppMetadata.supportedChannels[2].id).toBe("display");
+    expect(AppMetadata.supportedChannels[2].name).toBe("Display");
+    expect(AppMetadata.extensionId).toBe("extensionId");
   });
 });
